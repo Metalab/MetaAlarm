f15f0f48cfab10f8f430501c8f3dae46e3430156
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-07-30 12:24:42 +0200

    daily reboot

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "@reboot root sleep 20 && hostname -I | mail -s metaalarm sebastian.pfeifer@unicorncloud.org" | sudo tee /etc/cron.d/ip
 echo "* * * * * root bash /home/spfeifer/projects/metaalarm/smsparser.sh" | sudo tee /etc/cron.d/smsparser
 echo "@reboot root cd /home/spfeifer/projects/metaalarm/relaisboardsteuerung && ./usb-relay" | sudo tee /etc/cron.d/relaysoffatreboot
+echo "0 0 * * * root reboot" | sudo tee /etc/cron.d/dailyreboot
 sudo apt install smstools procmail -y
 sudo systemctl enable smstools.service
 sudo rm /etc/smsd.conf
